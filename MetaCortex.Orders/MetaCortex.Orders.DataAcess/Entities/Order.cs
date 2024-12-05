bf1d2c461bd346ff03d7da0e71bb09d4e20cbe45
Anders Ö <anders.oberg@iths.se>
2024-12-05 12:05:53 +0100

    added orders to microservices

---
+++ MetaCortex.Orders/MetaCortex.Orders.DataAcess/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MetaCortex.Orders.DataAcess.Entities
+{
+    public class Order : EntityBase
+    {
+        public DateTime OrderDate { get; set; }
+        public string CustomerId { get; set; }
+        public string PaymentMethod { get; set; }
+        public bool IsPaid { get; set; }
+        public bool VIPStatus { get; set; }
+        public List<string> Products { get; set; }
+    }
+}
